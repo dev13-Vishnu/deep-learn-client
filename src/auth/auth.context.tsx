692039e05f737bb1304ef4730e5548ec7257c5d0
Vishnu P S <vishnu13three@gmail.com>
2026-02-03 17:21:20 +0530

    feat(auth): add role context to auth interface

--- src/auth/auth.context.tsx
+++ src/auth/auth.context.tsx
@@ -1,12 +1,27 @@
 import { createContext } from 'react';
+export type RoleContext = 'student' | 'instrucctor' | 'admin'
 
 export interface AuthContextValue {
   user: any | null;
   isAuthenticated: boolean;
   isHydrating: boolean;
-  login: (email: string, password: string) => Promise<void>;
-  logout: () => void;
-  authenticateWithToken: (token: string) => void;
+
+  currentRole: RoleContext | null;
+
+  login: (
+    email: string,
+    password: string,
+    roleContext: RoleContext
+  ) => Promise<void>;
+
+
+  
+  authenticateWithToken: (
+    token: string,
+    roleContext: RoleContext
+  ) => void;
+
+  logout: () => Promise<void>;
 }
 
 export const AuthContext = createContext<AuthContextValue | null>(null);
